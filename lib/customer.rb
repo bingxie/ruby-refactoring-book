2008752ad42f614044f6258f077f4689030b4e39
Bing Xie <bing708@gmail.com>
2020-07-12 15:40:04 +1000

    Page17 eliminate this_amount by using Replace Temp with Query
    While refactoring you should focus on clarity, and then later focus on performance as a separate activity.

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,16 +17,13 @@ class Customer
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
-      # determine amounts for each line
-      this_amount = element.charge
-
       # add frequent renter points
       frequent_renter_points += 1
       # add bonus for a two day new release rental
       frequent_renter_points += 1 if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+      total_amount += element.charge
     end
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
