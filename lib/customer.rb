f55b3d68a5b7efa11433c7492fb609ec596bd056
Bing Xie <bing708@gmail.com>
2020-07-12 15:50:07 +1000

    Page19 Refactoring extract method #frequent_renter_points

--- lib/customer.rb
+++ lib/customer.rb
@@ -18,9 +18,8 @@ class Customer
 
     @rentals.each do |element|
       # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      frequent_renter_points += 1 if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      frequent_renter_points += element.frequent_renter_points
+      
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
       total_amount += element.charge
@@ -30,4 +29,6 @@ class Customer
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
+
+
 end
