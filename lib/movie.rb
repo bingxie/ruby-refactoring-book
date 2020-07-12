965dd679e9b42291b806b9cdc998408ba853199a
Bing Xie <bing708@gmail.com>
2020-07-12 16:31:52 +1000

    Page33 Move case statments to Movie class

--- lib/movie.rb
+++ lib/movie.rb
@@ -12,4 +12,19 @@ class Movie
     @title = title
     @price_code = price_code
   end
+
+  def charge(days_rented)
+    result = 0
+
+    case price_code
+    when REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when NEW_RELEASE
+      result += days_rented * 3
+    when CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+  end
 end
