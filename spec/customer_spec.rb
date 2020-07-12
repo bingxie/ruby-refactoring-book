695e741e0e641c295f72ad0bcc734447d0aac255
Bing Xie <bing708@gmail.com>
2020-07-12 16:21:48 +1000

    Page30 Add new #html_statement method and test

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -8,6 +8,8 @@ describe Customer do
       customer.add_rental(rental)
 
       expect(customer.statement).to eq "Rental Record for Bing\n\tABC\t15\nAmount owed is 15\nYou earned 2 frequent renter points"
+
+      expect(customer.html_statement).to eq "<h1>Rentals for <em>Bing</em></h1><p>\n\tABC: 15<br>\n<p>You owe <em>15</em><p>\nOn this rental you earned <em>2</em> frequent renter points<p>"
     end
   end
 end
